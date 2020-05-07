681e487b080b82f82eec23f0fdcf5ba5c9884aa3
Bow Ridinger <bowridinger@gmail.com>
2020-05-06 18:30:07 -0700

    continued work on a exec that runs all of our functions, this one kind of works

---
+++ Bowman/Function1_2_3.py
@@ -0,0 +1,133 @@
+# %%
+#Function 1 of ChemLibre Texts reading program, takes in a url, path, and browser type and returns the html
+#Path location should be in the format ex. C:/Users/bowri/Anaconda3/chromedriver
+#If using Firefox, or not Chrome, simply enter "" for path location, requires having downloaded chromedriver first
+#See formatting below
+
+#Stuff to do:
+    #1) throw more errors  - check, still work on the try/except for selenium being present
+    #2) getting rid of import functions - check
+    #3) add docstrings to let user know which types of data are allowed - check
+    #4) add default settings, eg. output = none; have output in, maybe more
+    #5) document better
+    
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import json
+import pandas as pd
+from lxml import html
+import selenium
+from bs4 import BeautifulSoup as BS
+import random
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import StaleElementReferenceException as SERE
+
+def selenium_html_collector(url, browser, path_to_driver, webdriver):
+    """"This function takes in three strings: 1) a url, 2) a browser,
+        and 3) a path to your local chromedriver location, which is only 
+        need if you are using Chrome. It takes in 4) a webdriver module
+        from Selenium as well. It returns an html of the given
+        url and opens the browser to that site as well""" 
+    
+    if browser == "Firefox":
+        #try:
+        drive = webdriver.Firefox()
+        #except: 
+        #    print("Did you import the webdriver module from Selenium?")   
+        
+    elif browser == "Chrome":
+        #try:
+        drive = webdriver.Chrome(executable_path= (path_to_driver))
+        #except: 
+        #    print("Did you import the webdriver module from Selenium?")
+
+        
+    elif browser != "Chrome" or "Firefox":
+        print("this is the weird browser:", browser)
+        raise Exception("Sorry, the function only utilizes Firefox and Chrome currently")
+
+        
+    drive.get(url)
+    return drive
+
+
+
+# %%
+#Test Runs
+#import selenium
+#from selenium import webdriver
+#from selenium.webdriver.common.keys import Keys
+
+#selenium_html_collector("https://www.wikipedia.org/", "Chrome", "C:/Users/bowri/Anaconda3/chromedriver", webdriver)
+#selenium_html_collector("https://www.wikipedia.org/", "Firefox", "")
+
+
+# %%
+#Function 3 of ChemLibreTexts reading program, takes in two lists: 1) chapter titles and 2) chapter 
+#contents and 3) a filename, and exports them to a JSON file with the given filename
+
+#Creates a dictionary with the two lists, and writes and opens a json file
+
+#add additional arguments for default settings, eg output_state boolean, for printing vs writing
+def chapter_exporter(chapter_titles, chapter_contents, filename, export = True):
+    """"This function takes in three variables, and has one default variable. The first two
+    variables must be lists, which ultimately get combined into a dictionary. The third var
+    is the string filename of your choice, and the final variable determines whether or not
+    the program will print or export the dictionary to a json. By default it is set to true"""
+    
+    if isinstance(chapter_titles, list) and isinstance(chapter_contents, list)  == True:
+       
+        titles_and_contents = dict(zip(chapter_titles, chapter_contents))
+
+        if export == True:
+            with open(filename, "w") as outfile:
+                json.dump(titles_and_contents, outfile)
+        else:
+            print(titles_and_contents)
+    else:
+         raise Exception("Variables passed in must be lists")
+
+# %%
+#import json
+#titles_list = ["chapter 1", "chapter 2", "chapter 3"]
+#chap_list = ["this is chapter 1", "this is chapter 2", "this is chapter 3"]
+#title = "chapter 1"
+
+#chapter_exporter(titles_list, chap_list, "test_chapter_writing", False)
+
+# %%
+def chapter_text_parser(driver):
+
+    driver.implicitly_wait(random.randint(1,100))
+    chapter_title = driver.find_element(By.XPATH, '//*[@id="title"]').text.strip()
+    subchap_link_title_container = driver.find_elements(By.CLASS_NAME, 'mt-listing-detailed-title')
+    subchap_titles = [title.text.strip() for title in subchap_link_title_container ]
+    subchap_links = [link.find_element_by_tag_name('a').get_attribute('href') for link in subchap_link_title_container]
+    print('Name of chapter', chapter_title, '\n', 'Number of subchapter is', len(subchap_links))
+    subchap_overview_container = driver.find_elements(By.CLASS_NAME, 'mt-listing-detailed-overview')
+    subchap_overviews = [overview.text.strip() for overview in subchap_overview_container]
+
+    subchap_contents=[]
+    data = {}
+    for chap_link in subchap_links:
+        driver.get(chap_link)
+        driver.page_source
+        chap_text_container = driver.find_elements(By.CLASS_NAME,'mt-content-container') 
+        for subchap in chap_text_container:
+            subchap_contents.append(subchap.text.strip())     
+    data = {'chap-title':subchap_titles, 'chap-overview': subchap_overviews, 'chap-content':subchap_contents}
+    return data
+
+# %%
+def new_exporter(dictionary, filename, driver, printout = False):
+    
+    if printout == False:
+        with open(filename, "w") as outfile:
+            json.dump(dictionary, outfile)
+    
+    else:
+         print(dictionary)   
+    return driver.close()
+    
--- Bowman/module_test.py
+++ Bowman/module_test.py
@@ -4,32 +4,59 @@
    "cell_type": "code",
    "execution_count": 3,
    "metadata": {},
+   "outputs": [],
+   "source": [
+    "from selenium import webdriver\n",
+    "from selenium.webdriver.common.keys import Keys\n",
+    "import json\n",
+    "import pandas as pd\n",
+    "from lxml import html\n",
+    "import selenium\n",
+    "from bs4 import BeautifulSoup as BS\n",
+    "import random\n",
+    "import time\n",
+    "from selenium.webdriver.common.by import By\n",
+    "from selenium.webdriver.support import expected_conditions as EC\n",
+    "from selenium.common.exceptions import StaleElementReferenceException as SERE\n",
+    "\n",
+    "import Function1_2_3 as BowFunks  #this is locally on my computer, so just need to figure out a wait to generalize an import\n",
+    "\n",
+    "#Pseudocode her:\n",
+    "#first ask \"user\" for an initial url, basically the one thing we need to run the program\n",
+    "\n",
+    "#Questions:\n",
+    "#determine how many json's we're exporting?\n",
+    "#Where do the other import things need to be, in the .py module that we're importing? or in this master function\n",
+    "\n",
+    "#Basically summary:\n",
+    "\"\"\"This program so far imports a variant of the functions we all made as BowFunks. It then takes a given start url, in this case a Chapter from\n",
+    "the Schaller textbook, then goes through each subchapter(ie 1.1 etc) and gets all the content. Right now it just runs through one chapter. \n",
+    "Trying to broaden that.\"\"\""
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 5,
+   "metadata": {},
    "outputs": [
     {
      "name": "stdout",
      "output_type": "stream",
      "text": [
-      "{'chapter 1': 'this is chapter 1', 'chapter 2': 'this is chapter 2', 'chapter 3': 'this is chapter 3'}\n"
+      "Name of chapter 1: Monomers and Polymers \n",
+      " Number of subchapter is 9\n"
      ]
     }
    ],
    "source": [
-    "from selenium import webdriver\n",
-    "from selenium.webdriver.common.keys import Keys\n",
-    "import json\n",
-    "import Function1_3 as BowFunks\n",
+    "url = \"https://chem.libretexts.org/Bookshelves/Organic_Chemistry/Book%3A_Polymer_Chemistry_(Schaller)/1%3A_Monomers_and_Polymers\"\n",
     "\n",
-    "#write pseudocode here\n",
-    "#first ask \"user\" for an initial url, basically the one thing we need to run the program\n",
-    "#determine how many json's we're exporting?\n",
+    "html_start = BowFunks.selenium_html_collector(url, \"Chrome\", \"C:/Users/bowri/Anaconda3/chromedriver\", webdriver)\n",
     "\n",
-    "BowFunks.selenium_html_collector(\"https://www.wikipedia.org/\", \"Chrome\", \"C:/Users/bowri/Anaconda3/chromedriver\", webdriver)\n",
+    "all_dict = BowFunks.chapter_text_parser(html_start)\n",
     "\n",
-    "titles_list = [\"chapter 1\", \"chapter 2\", \"chapter 3\"]\n",
-    "chap_list = [\"this is chapter 1\", \"this is chapter 2\", \"this is chapter 3\"]\n",
-    "title = \"chapter 1\"\n",
-    "\n",
-    "BowFunks.chapter_exporter(titles_list, chap_list, \"test_chapter_writing\", False)\n"
+    "BowFunks.new_exporter(all_dict, \"test_exe\", html_start, False)\n",
+    "\n"
    ]
   },
   {
